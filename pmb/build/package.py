2e4c591d8eab1aa67b92be1852e7cbcf588c25df
Oliver Smith <ollieparanoid@bitmessage.ch>
2017-11-08 21:10:56 +0100

    WIP: refactor pmb/build/package.py
    It was one big function, that did too many things. I'm refactoring
    it, so we can extend it sanely (e.g. with native cross-compilation
    for autoconf packages #665).
    
    What's done:
    * package() function has been split up in more small ones
    * confusing terms have been renamed: carch -> arch
    * no more _buildenv variables, they weren't really helpful in my
      opinion
    * code seems to work with some basic testing
    
    TODO:
    * testcases
    * more comments

--- pmb/build/package.py
+++ pmb/build/package.py
@@ -29,79 +29,85 @@ import pmb.parse
 import pmb.parse.arch
 
 
-def package(args, pkgname, carch, force=False, buildinfo=False, strict=False):
+def get_apkbuild(args, pkgname, arch):
     """
-    Build a package with Alpine Linux' abuild.
+    Find the APKBUILD path for pkgname. When there is none, try to find it in
+    the binary package APKINDEX files or raise an exception.
 
-    :param force: even build, if not necessary
-    :returns: output path relative to the packages folder
+    :param pkgname: package name to be built, as specified in the APKBUILD
+    :returns: None or full path to APKBUILD
     """
-    # Get aport, skip upstream only packages
     aport = pmb.build.find_aport(args, pkgname, False)
-    if not aport:
-        if pmb.parse.apkindex.read_any_index(args, pkgname, carch):
-            return
-        raise RuntimeError("Package " + pkgname + ": Could not find aport,"
-                           " and could not find this package in any APKINDEX!")
+    if aport:
+        return pmb.parse.apkbuild(args, aport + "/APKBUILD")
+    if pmb.parse.apkindex.read_any_index(args, pkgname, arch):
+        return None
+    raise RuntimeError("Package '" + pkgname + "': Could not find aport, and"
+                       " could not find this package in any APKINDEX!")
 
-    # Autodetect the build environment
-    apkbuild = pmb.parse.apkbuild(args, aport + "/APKBUILD")
-    pkgname = apkbuild["pkgname"]
-    carch_buildenv = pmb.build.autodetect.carch(args, apkbuild, carch, strict)
-    suffix = pmb.build.autodetect.suffix(args, apkbuild, carch_buildenv)
-    cross = pmb.build.autodetect.crosscompile(args, apkbuild, carch_buildenv,
-                                              suffix)
 
-    # Skip already built versions
-    if not force and not pmb.build.is_necessary(args, carch_buildenv, apkbuild):
-        return
+def build_dependencies(args, apkbuild, arch, strict, force, suffix):
+    # Is the build necessary? (before installing dependencies)
+    is_necessary_pre_depends = pmb.build.is_necessary(args, arch, apkbuild)
+    if not force and not is_necessary_pre_depends:
+        return True
 
-    # Initialize build environment, install/build makedepends
-    pmb.build.init(args, suffix)
-    if len(apkbuild["makedepends"]):
-        if strict:
-            for makedepend in apkbuild["makedepends"]:
-                package(args, makedepend, carch_buildenv, strict=True)
-        else:
-            pmb.chroot.apk.install(args, apkbuild["makedepends"], suffix)
-    if cross:
-        pmb.chroot.apk.install(args, ["gcc-" + carch_buildenv,
-                                      "g++-" + carch_buildenv,
-                                      "ccache-cross-symlinks"])
-        if cross == "distcc":
-            pmb.chroot.apk.install(args, ["distcc"], suffix=suffix,
-                                   build=False)
-            pmb.chroot.distccd.start(args, carch_buildenv)
+    # Build/install dependencies
+    if strict:
+        for depend in apkbuild["makedepends"] + apkbuild["depends"]:
+            package(args, depend, arch, strict=True)
+    elif len(apkbuild["makedepends"]):
+        pmb.chroot.apk.install(args, apkbuild["makedepends"], suffix)
 
     # Avoid re-building for circular dependencies
-    if not force and not pmb.build.is_necessary(args, carch, apkbuild):
-        return
+    if not pmb.build.is_necessary(args, arch, apkbuild):
+        if force and not is_necessary_pre_depends:
+            return True
 
-    # Configure abuild.conf
+
+def init_buildenv(args, apkbuild, arch, strict, force, cross, suffix):
+    # Build and install dependencies (don't rebuild circular depends)
+    if build_dependencies(args, apkbuild, arch, strict, force, suffix):
+        return True
+
+    # Install and configure abuild and gcc
+    pmb.build.init(args, suffix)
     pmb.build.other.configure_abuild(args, suffix)
 
-    # Generate output name, log build message
-    output = (carch_buildenv + "/" + apkbuild["pkgname"] + "-" +
-              apkbuild["pkgver"] + "-r" + apkbuild["pkgrel"] + ".apk")
-    logging.info("(" + suffix + ") build " + output)
+    # Cross-compiler init
+    if cross:
+        pmb.chroot.apk.install(args, ["gcc-" + arch, "g++-" + arch,
+                                      "ccache-cross-symlinks"])
+    if cross == "distcc":
+        pmb.chroot.apk.install(args, ["distcc"], suffix=suffix,
+                               build=False)
+        pmb.chroot.distccd.start(args, arch)
 
+
+def run_abuild(args, apkbuild, arch, strict, force, cross, suffix):
     # Sanity check
     if cross == "native" and "!tracedeps" not in apkbuild["options"]:
         logging.info("WARNING: Option !tracedeps is not set, but we're"
-                     " cross-compiling in the native chroot. This will probably"
-                     " fail!")
+                     " cross-compiling in the native chroot. This will"
+                     " probably fail!")
 
-    # Run abuild
-    pmb.build.copy_to_buildpath(args, pkgname, suffix)
-    cmd = []
-    env = {"CARCH": carch_buildenv}
+    # Pretty log message
+    output = (arch + "/" + apkbuild["pkgname"] + "-" + apkbuild["pkgver"] +
+              "-r" + apkbuild["pkgrel"] + ".apk")
+    logging.info("(" + suffix + ") build " + output)
+
+    # Environment variables
+    env = {"CARCH": arch}
     if cross == "native":
-        hostspec = pmb.parse.arch.alpine_to_hostspec(carch_buildenv)
+        hostspec = pmb.parse.arch.alpine_to_hostspec(arch)
         env["CROSS_COMPILE"] = hostspec + "-"
         env["CC"] = hostspec + "-gcc"
     if cross == "distcc":
         env["PATH"] = "/usr/lib/distcc/bin:" + pmb.config.chroot_path
         env["DISTCC_HOSTS"] = "127.0.0.1:" + args.port_distccd
+
+    # Build the abuild command
+    cmd = []
     for key, value in env.items():
         cmd += [key + "=" + value]
     cmd += ["abuild"]
@@ -111,8 +117,14 @@ def package(args, pkgname, carch, force=False, buildinfo=False, strict=False):
         cmd += ["-d"]  # do not install depends with abuild
     if force:
         cmd += ["-f"]
-    pmb.chroot.user(args, cmd, suffix, "/home/pmos/build")
 
+    # Copy the aport to the chroot and build it
+    pmb.build.copy_to_buildpath(args, apkbuild["pkgname"], suffix)
+    pmb.chroot.user(args, cmd, suffix, "/home/pmos/build")
+    return output
+
+
+def clean_up(args, apkbuild, arch, buildinfo, strict, suffix, output):
     # Verify output file
     path = args.work + "/packages/" + output
     if not os.path.exists(path):
@@ -121,8 +133,7 @@ def package(args, pkgname, carch, force=False, buildinfo=False, strict=False):
     # Create .buildinfo.json file
     if buildinfo:
         logging.info("(" + suffix + ") generate " + output + ".buildinfo.json")
-        pmb.build.buildinfo.write(args, output, carch_buildenv, suffix,
-                                  apkbuild)
+        pmb.build.buildinfo.write(args, output, arch, suffix, apkbuild)
 
     # Symlink noarch package (and subpackages)
     if "noarch" in apkbuild["arch"]:
@@ -130,9 +141,39 @@ def package(args, pkgname, carch, force=False, buildinfo=False, strict=False):
 
     # Clean up (APKINDEX cache, depends when strict)
     pmb.parse.apkindex.clear_cache(args, args.work + "/packages/" +
-                                   carch_buildenv + "/APKINDEX.tar.gz")
+                                   arch + "/APKINDEX.tar.gz")
     if strict:
         logging.info("(" + suffix + ") uninstall makedepends")
         pmb.chroot.user(args, ["abuild", "undeps"], suffix, "/home/pmos/build")
 
+
+def package(args, pkgname, arch, force=False, buildinfo=False, strict=False):
+    """
+    Build a package with Alpine Linux' abuild.
+
+    :param pkgname: package name to be built, as specified in the APKBUILD
+    :param arch: architecture we're building for
+    :param force: even build, if not necessary
+    :param buildinfo: record the build environment in a .buildinfo.json file
+    :param strict: avoid building with irrelevant dependencies installed by
+                   letting abuild install and uninstall all dependencies. It
+                   also installs the depends, and not only the makedepends like
+                   pmbootstrap does normally to improve performance.
+    :returns: output path relative to the packages folder
+    """
+    # Only build when APKBUILD exists
+    apkbuild = get_apkbuild(args, pkgname, arch)
+    if not apkbuild:
+        return
+
+    # Set up the build environment (don't rebuild circular depends)
+    arch = pmb.build.autodetect.arch(args, apkbuild, arch, strict)
+    suffix = pmb.build.autodetect.suffix(args, apkbuild, arch)
+    cross = pmb.build.autodetect.crosscompile(args, apkbuild, arch, suffix)
+    if init_buildenv(args, apkbuild, arch, strict, force, cross, suffix):
+        return
+
+    # Build and clean up
+    output = run_abuild(args, apkbuild, arch, strict, force, cross, suffix)
+    clean_up(args, apkbuild, arch, buildinfo, strict, suffix, output)
     return output
